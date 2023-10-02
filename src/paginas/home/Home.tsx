b740471316d62105cefa9cea4e1fab544e613439
prrrri <priscilaigarashi@icloud.com>
2023-10-02 18:52:17 -0300

    props e hooks

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,17 +1,16 @@
-import React from "react";
-import "./Home.css";
+import React, { useState } from "react";
 
 function Home() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
-    <>
-      <h1 className="titulo">Home</h1>
-      <img
-        src="https://i.imgur.com/H88yIo2.png"
-        alt="Imagem Tela Inicial"
-        className="img"
-      />
-    </>
+    <div>
+      {loggedIn ? (
+        <h1>Bem-vindo de volta!</h1>
+      ) : (
+        <button onClick={() => setLoggedIn(true)}>Entrar</button>
+      )}
+    </div>
   );
 }
-
 export default Home;
