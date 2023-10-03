fb131b7e0727393b2393db40fe7d86d34f8cf8b5
prrrri <priscilaigarashi@icloud.com>
2023-10-03 11:41:38 -0300

    Criando Rotas com o React Router Dom

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import React from "react";
 
-function Home() {
-  const [loggedIn, setLoggedIn] = useState(false);
-
+const Home = () => {
+  let navigate = useNavigate();
   return (
     <div>
-      {loggedIn ? (
-        <h1>Bem-vindo de volta!</h1>
-      ) : (
-        <button onClick={() => setLoggedIn(true)}>Entrar</button>
-      )}
+      <h2 className="text-slate-900 text-5xl  m-4">Home</h2>
+      <div>
+        <button
+          type="submit"
+          className="hover:underline mx-4"
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          Login useNavigate
+        </button>
+        <Link to="/login" className="hover:underline mx-4">
+          Login por Link
+        </Link>
+      </div>
     </div>
   );
-}
+};
+
 export default Home;
