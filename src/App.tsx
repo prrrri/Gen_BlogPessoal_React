54098de775fa3491bdb8ad891c9d5f64825c61d8
prrrri <priscilaigarashi@icloud.com>
2023-10-07 10:02:33 -0300

    cadastro de usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import React from "react";
-import UserProvider from "./contexts/UserContext";
+import "./App.css";
+
+import Navbar from "./components/navBar/Navbar";
+import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
+import Cadastro from "./paginas/cadastro/Cadastro";
+import Home from "./paginas/home/Home";
 
 function App() {
   return (
-    <UserProvider>
+    <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
-    </UserProvider>
+    </>
   );
 }
-
 export default App;
