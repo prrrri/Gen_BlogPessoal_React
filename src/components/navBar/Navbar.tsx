e1cab53c18696e05184e48564bb401e7f75bcbd3
prrrri <priscilaigarashi@icloud.com>
2023-10-07 08:49:25 -0300

    criando as rotas

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link, useNavigate } from "react-router-dom";
 import React from "react";
 
 function Navbar() {
@@ -8,6 +9,12 @@ function Navbar() {
           <div className="text-2xl font-bold uppercase">Blog Pessoal</div>
 
           <div className="flex gap-4">
+            <Link to="/login" className="hover:underline">
+              Login
+            </Link>
+            <Link to="/home" className="hover:underline">
+              Home
+            </Link>
             <div className="hover:underline">Postagens</div>
             <div className="hover:underline">Temas</div>
             <div className="hover:underline">Cadastrar tema</div>
