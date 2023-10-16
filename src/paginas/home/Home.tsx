12f574bfe5e0ea1d9be1ece5818a389163775c08
prrrri <priscilaigarashi@icloud.com>
2023-10-16 14:26:02 -0300

    construindo o componente perfil

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-//import homeLogo from "../../assets/home.png";
+import homeLogo from "../../assets/home.png";
 import "./Home.css";
 import ListaPostagens from "../../components/postagens/listaPostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
 
 function Home() {
   return (
@@ -10,9 +11,10 @@ function Home() {
         <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
             <h2 className="text-5xl font-bold">Seja bem vinde!</h2>
-            <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
+            <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
 
             <div className="flex justify-around gap-4">
+              <ModalPostagem />
               <button className="rounded bg-white text-blue-800 py-2 px-4">
                 Ver postagens
               </button>
